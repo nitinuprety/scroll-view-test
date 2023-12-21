20b7e552e109c7e97994ab1f7776c6aa2cf55903
nitin <nuprety2@gmail.com>
2023-12-21 12:43:30 +0530

    add2

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 var frameNumber = 0, // start video at frame 0
+ 
     // lower numbers = faster playback
     playbackConst = 800, 
     // get page height from video duration
@@ -8,12 +9,13 @@ var frameNumber = 0, // start video at frame 0
 
 // dynamically set the page height according to video length
 vid.addEventListener('loadedmetadata', function() {
+  console.log("FRAME:", frameNumber);
   setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
 });
 
 // Use requestAnimationFrame for smooth playback
 function scrollPlay(){  
-  var frameNumber  = window.scrollY/TouchEvent/playbackConst;
+  var frameNumber  = window.scrollY/playbackConst;
   vid.currentTime  = frameNumber;
   window.requestAnimationFrame(scrollPlay);
 }
